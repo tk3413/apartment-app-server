d38000895c1a9f0309c60134c7ffab1445ec022d
tk3413 <khan.taimore@gmail.com>
2020-02-05 22:08:43 -0500

    added typedefs and resolvers

---
+++ graphql/resolvers.js
@@ -0,0 +1,50 @@
+const apartments = require('../models').apartment
+const Sequelize = require('sequelize')
+const Op = Sequelize.Op
+
+module.exports = resolvers = {
+  Query: {
+    async apartmentByAptNum (obj, args, context, info) {
+      return await apartments.gqlFindByAptNum(args.apt_num)
+    },
+
+    async countGroupByAptSize (obj, args, context, info) {
+      return apartments
+        .count({
+          group: ['apt_size']
+        })
+        .then(count => {
+          console.log(count)
+          return count
+        })
+    },
+
+    async countGroupByVitaAptNum (obj, args, context, info) {
+      return apartments
+        .count({
+          group: [Sequelize.fn('RIGHT', Sequelize.col('apt_num'), 2)]
+        })
+        .then(count => {
+          console.log(count)
+          return count
+        })
+    },
+
+    async averagePrice (obj, args, context, info) {
+      return apartments.count({
+        attributes: [
+          Sequelize.fn('AVG', Sequelize.col('apt_prices')),
+          Sequelize.fn('RIGHT', Sequelize.col('apt_num'), 2),
+          'apt_size'
+        ],
+        where: {
+          [Op.and]: [
+            { apt_nm_cd: { [Op.eq]: args.apt_nm_cd } },
+            { apt_prices: { [Op.gt]: 1 } }
+          ]
+        },
+        group: ['apt_size', Sequelize.fn('RIGHT', Sequelize.col('apt_num'), 2)]
+      })
+    }
+  }
+}
---
+++ graphql/typedefs.js
@@ -0,0 +1,40 @@
+const gql = require('graphql-tag')
+
+const typeDefs = gql`
+  type Query {
+    apartmentByAptNum(apt_nm_cd: String!, apt_num: String): [Apartment]
+
+    countGroupByAptSize(apt_nm_cd: String): [Vita_Apt_Num_Count]
+
+    countGroupByVitaAptNum: [Vita_Apt_Num_Count]
+    averagePrice(apt_nm_cd: String): [AveragePrice]
+  }
+
+  type Apartment {
+    id: ID
+    apt_nm_cd: String
+    apt_num: String
+    apt_type: String
+    apt_size: String
+    apt_price: String
+    apt_avl_dt: String
+    cret_ts: String
+    updt_ts: String
+  }
+
+  type Vita_Apt_Num_Count {
+    right: String
+    apt_size: String
+    apt_type: String
+    count: Int
+  }
+
+  type AveragePrice {
+    avg: Float
+    right: String
+    count: Int
+    apt_size: String
+  }
+`
+
+module.exports = typeDefs
