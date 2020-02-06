aa57c7ae7a12ca596d904b8b938f4ed004ec5b28
tk3413 <khan.taimore@gmail.com>
2020-02-05 22:08:21 -0500

    added graphql layer

--- index.js
+++ index.js
@@ -2,18 +2,24 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
+const { ApolloServer, gql } = require('apollo-server-express')
+
+const typeDefs = require('./graphql/typedefs')
+const resolvers = require('./graphql/resolvers')
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app });
 
 const apartments = require('./controllers/apartment')
 const info = require('./controllers/info')
-
-app.use(bodyParser.json())
 
 // apartments
 app.get('/apartments', apartments.findAll)
 app.get('/apartments/:apt_num', apartments.findByAptNum)
 app.post('/apartments', apartments.create)
 app.delete('/apartments', apartments.delete) // only for integration tests
-
 
 // apartment info
 app.get('/apartment-info', info.findAll)
@@ -26,4 +32,4 @@ app.get('/health', (req, res) => {
     res.sendStatus(200)
 })
 
-app.listen(port, () => console.log(`apartment-app listening on port ${port}!`))
+app.listen(port, () => console.log(`apartment-app listening on port ${port}, graphql: ${server.graphqlPath}!`))
